969db1a393bb6f09720ab861650afa451540b373
tiwaripraveen <tiwaripraveen.2006@gmail.com>
2026-08-27 11:53:46 +0530

    first commit

---
+++ HelloWorldUsingOjectAndMethodCalling.java
@@ -0,0 +1,13 @@
+class Hello{
+    void display(){
+        System.out.println("Hello World");
+    }
+}
+public class HelloWorldUsingOjectAndMethodCalling {
+    public static void main(String[] args) {
+        Hello obj=new Hello();
+        obj.display();
+        
+    }}
+    
+
