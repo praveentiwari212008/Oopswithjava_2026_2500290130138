a1b1b7e7b88a1b5a38f035f69f54e57e7ea52ff8
tiwaripraveen <tiwaripraveen.2006@gmail.com>
2026-09-21 22:13:40 +0530

    Practice

---
+++ oopswithjava_project/src/InterfacesPractice.java
@@ -0,0 +1,22 @@
+public class InterfacesPractice {
+    public static void main(String[] args){
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        a1.sound();
+        a2.sound();
+    }
+    
+}
+interface  Animal{
+    void sound();
+}
+class Dog implements Animal{
+    public void sound(){
+         System.out.println("Dog barks: ");
+    }
+}
+class Cat implements Animal{
+   public void sound(){
+         System.out.println("Cat meow: ");
+    }
+}
