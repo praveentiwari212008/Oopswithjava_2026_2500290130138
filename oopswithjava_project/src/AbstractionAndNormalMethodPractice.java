f7e4f5a68800823461d7c24fe25e964d17d40340
tiwaripraveen <tiwaripraveen.2006@gmail.com>
2026-09-21 19:55:02 +0530

    Practice

--- oopswithjava_project/src/AbstractionAndNormalMethodPractice.java
+++ oopswithjava_project/src/AbstractionAndNormalMethodPractice.java
@@ -1,3 +1,30 @@
 public class AbstractionAndNormalMethodPractice {
+    public static void main(String[] args) {
+    Vehicle obj1 = new Car();
+    obj1.start();
+    obj1.stop();
+         Vehicle obj2 = new Bike();
+    obj2.start();
+    obj2.stop();
+    }
     
 }
+abstract class Vehicle{
+    abstract void start();
+    void stop(){
+        System.out.println("Vehicle stopped: ");
+    }
+}
+class Car extends Vehicle{
+   @Override 
+    void start() {
+        System.out.println("Car started: ");
+    }
+}
+class Bike extends Vehicle{
+ @Override 
+    void start() {
+        System.out.println("Bike started: ");
+    }
+}
+ 
